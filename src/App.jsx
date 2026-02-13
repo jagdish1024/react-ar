c321583705b06f32a7c78186f887f65d6c0d50db
jagdish1024 <138677088+jagdish1024@users.noreply.github.com>
2026-02-13 11:38:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// src/App.jsx
+// src/App.jsx
+import ARCanvas from "./components/ARCanvas";
+
+export default function App() {
+  return <ARCanvas />;
+}
+
+
